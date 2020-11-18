472c8cd595c87ae361b7b2fd832d4abdbc0dda16
Picchi89 <brunof.picchi@gmai.com>
2020-11-18 18:56:29 -0300

    Alterado 18/11

---
+++ SiteViagensNovo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SiteViagensNovo.Models;
+
+namespace SiteViagensNovo.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Estamos trabalhando em nosso mais novo site! Fique atento!";
+
+            return View();
+        }
+
+        public IActionResult Contact() {
+            ViewData["Message"] = "Qualquer dúvida, não deixe de entrar em contato conosco!";
+
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SiteViagensNovo/Data/SiteViagensNovoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SiteViagensNovo.Models;
+
+namespace SiteViagensNovo.Data
+{
+    public class SiteViagensNovoContext : DbContext
+    {
+        public SiteViagensNovoContext (DbContextOptions<SiteViagensNovoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteViagensNovo.Models.Department> Department { get; set; }
+    }
+}
